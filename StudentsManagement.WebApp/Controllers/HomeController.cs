4db0f9a7d5d7558e20964fb69604aec37d0ef4ac
phithanh1230 <phiphi7430@gmail.com>
2018-08-27 22:40:11 +0700

    Commit V2

--- StudentsManagement.WebApp/Controllers/HomeController.cs
+++ StudentsManagement.WebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentsManagement.DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,12 @@ namespace StudentsManagement.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        //
+        // GET: /Home/
         public ActionResult Index()
         {
+            StudentManagementEntities db = new StudentManagementEntities();
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
+	}
 }
